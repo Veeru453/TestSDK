05b0b4eda6c0c64ace61073b8af56f3859521131
Veeru <Veerendranath.Rangapuri@Honeywell.com>
2023-08-18 10:56:23 -0400

    Initial repo update

---
+++ TestSDK/src/main/java/com/veeru/testsdk/MathUtil.java
@@ -0,0 +1,11 @@
+package com.veeru.testsdk;
+
+public class MathUtil {
+    public float addNumbers(float a, float b){
+        return a+b;
+    }
+
+    public float multiplyNumbers(float a, float b){
+        return a+b;
+    }
+}
